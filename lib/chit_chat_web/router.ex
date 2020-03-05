74dba02a6bdddc98702c1390bcd21d8b144f5ad7
kamranhossain <kamran.hossain227@gmail.com>
2020-03-05 21:06:58 +0600

    gen with html Accounts, user and Chat room, gen context credential

--- lib/chit_chat_web/router.ex
+++ lib/chit_chat_web/router.ex
@@ -17,6 +17,8 @@ defmodule ChitChatWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/rooms", RoomController
+    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
